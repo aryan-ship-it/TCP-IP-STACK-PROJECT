d2b278226e3396d1b00ce755b9fb9a143bdd3b68
aryan <aryanjungshah881@gmail.com>
2023-04-13 14:47:39 -0500

    Ip adress

--- ComputerNetworking/ComputerNetworking.cpp
+++ ComputerNetworking/ComputerNetworking.cpp
@@ -7,36 +7,60 @@
 
 using namespace std;
 void parseMessage() {
-    fstream getFile;
-    getFile.open("getRequest.txt", ios::in); //reading from file gerRequest.txt
-    if (getFile.is_open()) {
-        string line;
-        while (getline(getFile, line)) {
-            string name;
-            string value;
-            size_t position = line.find("\r");
-            if (position != std::string::npos) {
-                name = line.substr(0, position);
-                value = line.substr(position + 1);
-            }
-            else {                
-                value = line;
-            }
+    ifstream getFile("getRequest.txt");
+    if (!getFile.is_open()) {
+        cout << "Unable to open file." << endl;
 
-            cout  << value << endl;
+        return;
+    }
+
+    const int NUM_LINES = 8;
+    string lines[NUM_LINES];
+    int i = 0;
+    string line;
+    while (getline(getFile, line)) {
+        if (i == 0) {
+            lines[i] = line.substr(0, line.find("HTTP") + 8);
         }
-        getFile.close();
+        else {
+            size_t position = line.find(':');
+            if (position != std::string::npos) {
+                lines[i] = line.substr(position + 2, line.length() - position - 6);
+            }
+        }
+        i++;
     }
-    else {
-        cout << "unable to open file" << endl;
+    getFile.close();
+
+    // print the lines in the array
+    for (int i = 0; i < NUM_LINES; i++) {
+        cout << lines[i] << endl;
     }
 
+    cout << lines[2];
 
+
+    return;
+
+}
+
+string IPAddress() {
+    srand(time(0)); // ensures that it is a different random number every time it runs
+    string address = "";
+    for (int i = 0; i < 4; i++) {
+        int octet = rand() % 256; // generate random number between 0 and 255
+        address += to_string(octet); // convert the number to a string and append it to the address
+        if (i < 3) {
+            address += "."; // add a dot separator between octets
+        }
+    }
+    cout << address;
+    return address;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    IPAddress();
     parseMessage();
 }
 
